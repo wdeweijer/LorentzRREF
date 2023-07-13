d198841bcdc8d710cc3c14ef1e364d4df75345ab
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-07-13 17:57:52 +0300

    Convert back and forth between ArrayMat and Matrix

--- LorentzRREF/RREF.lean
+++ LorentzRREF/RREF.lean
@@ -8,18 +8,25 @@ def findPivot (A : Fin R → K) (r : Fin (R + 1)) :
     Option (Fin R) :=
   Fin.find (fun i ↦ i ≥ r ∧ A i ≠ 0)
 
-def Matrix.RREFTransformation' {R C : ℕ}[ToString K] (A : ArrayMat R C K)
+def Matrix.RREFTransformation' {R C : ℕ}(A : ArrayMat R C K)
     (r : Fin (R + 1) := 0) :
     ArrayMat R R K :=
   match C with
   | 0 => 1
-  | C'+ 1 => dbgTrace ("Z: " ++ toString C' ++ toString A.1) fun _ => if h : r = Fin.last R
+  | C'+ 1 => if h : r = Fin.last R
     then 1 -- Done with all the rows, nothing to transform. 
     else ( let r : Fin R := ⟨r.val, by exact Fin.val_lt_last h⟩
       match findPivot (fun i => A.get_elem i 0) r with
       | .none => Matrix.RREFTransformation' (ArrayMat.dropFirstColumns 1 A) r
-      | .some pvt => dbgTrace ("C': " ++ toString C' ++ " / PVT: " ++ toString pvt) fun _ =>
+      | .some pvt =>
        let (A', T₁) := matrixRowSwap A pvt r
        let (A'', T₂) := matrixRowDilation A' r
        let (A''', T₃) := matrixRowTransvections A'' r
       (Matrix.RREFTransformation' (ArrayMat.dropFirstColumns 1 A''') (r + 1)) * T₃ * T₂ * T₁)
+
+def Matrix.RREFTransformation {R C : ℕ} (A: Matrix (Fin R) (Fin C) K) :
+  Matrix (Fin R) (Fin R) K := (Matrix.RREFTransformation' A.toArrayMat).toMatrix
+
+def Matrix.RREF {R C : ℕ} (A: Matrix (Fin R) (Fin C) K) :
+  Matrix (Fin R) (Fin C) K := 
+  ((Matrix.RREFTransformation' A.toArrayMat).mul A.toArrayMat).toMatrix
