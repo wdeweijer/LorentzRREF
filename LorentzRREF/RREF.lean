c6402a5b3af0794f792998c7accbe8e0ef895add
Wessel de Weijer <w.deweijer@hotmail.com>
2023-07-14 13:31:56 +0200

    Cleanup1

--- LorentzRREF/RREF.lean
+++ LorentzRREF/RREF.lean
@@ -7,7 +7,7 @@ def findPivot (A : Fin R → K) (r : Fin (R + 1)) :
     Option (Fin R) :=
   Fin.find (fun i ↦ i ≥ r ∧ A i ≠ 0)
 
-def Matrix.RREFTransformation' {R C : ℕ}(A : ArrayMat R C K)
+def ArrayMat.RREFTransformation' {R C : ℕ}(A : ArrayMat R C K)
     (r : Fin (R + 1) := 0) :
     ArrayMat R R K :=
   match C with
@@ -16,16 +16,16 @@ def Matrix.RREFTransformation' {R C : ℕ}(A : ArrayMat R C K)
     then 1 -- Done with all the rows, nothing to transform. 
     else ( let r : Fin R := ⟨r.val, by exact Fin.val_lt_last h⟩
       match findPivot (fun i => A.get_elem i 0) r with
-      | .none => Matrix.RREFTransformation' (ArrayMat.dropFirstColumns 1 A) r
+      | .none => ArrayMat.RREFTransformation' (ArrayMat.dropFirstColumns 1 A) r
       | .some pvt =>
        let (A', T₁) := matrixRowSwap A pvt r
        let (A'', T₂) := matrixRowDilation A' r
        let (A''', T₃) := matrixRowTransvections A'' r
-      (Matrix.RREFTransformation' (ArrayMat.dropFirstColumns 1 A''') (r + 1)) * T₃ * T₂ * T₁)
+      (ArrayMat.RREFTransformation' (ArrayMat.dropFirstColumns 1 A''') (r + 1)) * T₃ * T₂ * T₁)
 
 def Matrix.RREFTransformation {R C : ℕ} (A: Matrix (Fin R) (Fin C) K) :
-  Matrix (Fin R) (Fin R) K := (Matrix.RREFTransformation' A.toArrayMat).toMatrix
+  Matrix (Fin R) (Fin R) K := (ArrayMat.RREFTransformation' A.toArrayMat).toMatrix
 
 def Matrix.RREF {R C : ℕ} (A: Matrix (Fin R) (Fin C) K) :
-  Matrix (Fin R) (Fin C) K := 
-  ((Matrix.RREFTransformation' A.toArrayMat).mul A.toArrayMat).toMatrix
+    Matrix (Fin R) (Fin C) K := 
+  (A.toArrayMat.RREFTransformation'.mul A.toArrayMat).toMatrix
--- LorentzRREF/testRREF.lean
+++ LorentzRREF/testRREF.lean
@@ -1,12 +1,13 @@
 import LorentzRREF.RREF
 
-def wierdMat1 := (!![(1:ℚ), 3, 1, 9; 1, 1, -1, 1; 3, 11, 5, 37000000000000000000]).toArrayMat
-def wierdMat2 := (1: Matrix (Fin 3) (Fin 3) ℚ).toArrayMat
-def wierdMat3 := (0: Matrix (Fin 3) (Fin 4) ℚ).toArrayMat
-def wierdMat4 := (!![(0:ℚ), 0, 0, 1; 0, 0, 1, 0; 0, 1, 0, 0;]).toArrayMat
+def wierdMat1 := (!![(1:ℚ), 3, 1, 9; 1, 1, -1, 1; 3, 11, 5, 37000000000000000000])
 
-set_option trace.compiler true
-#eval wierdMat1
-#eval Matrix.RREFTransformation' wierdMat1
-#eval (Matrix.RREFTransformation' wierdMat1).mul wierdMat1
-#eval (Matrix.RREFTransformation' wierdMat4).mul wierdMat4
+def wierdMat2 := (1: Matrix (Fin 3) (Fin 3) ℚ)
+def wierdMat3 := (0: Matrix (Fin 3) (Fin 4) ℚ)
+def wierdMat4 := (!![(0:ℚ), 0, 0, 1; 0, 0, 1, 0; 0, 1, 0, 0;])
+
+-- set_option trace.compiler true
+#eval wierdMat1                                 -- Original Matrix
+#eval wierdMat1.RREFTransformation              -- Transformation Matrix
+#eval wierdMat1.RREF                            -- Reduced Row Echelon Form
+#eval wierdMat1.toArrayMat.RREFTransformation'  --
